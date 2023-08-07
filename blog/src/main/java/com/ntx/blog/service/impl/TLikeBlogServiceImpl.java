0eb781a5ff16e26af6d7f71c9a0db6d7f3a4c51e
zhangjize <z15588222448@gmail.com>
2023-08-07 14:11:36 +0800

    点赞功能bug修复

--- blog/src/main/java/com/ntx/blog/service/impl/TLikeBlogServiceImpl.java
+++ blog/src/main/java/com/ntx/blog/service/impl/TLikeBlogServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ntx.blog.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
@@ -38,9 +39,10 @@ public class TLikeBlogServiceImpl extends ServiceImpl<TLikeBlogMapper, TLikeBlog
         //目前有时间bug
         //判断用户是否点赞，如果点过赞，则取消点赞（删除），没点过赞，则新增点赞，如果用户之前的点赞状态是2，即反对，则取消反对，重新点赞
         //从redis中获取点赞数据，获取不到查数据库,如果没有数据，则代表之前用户没有给这篇博客点过赞
-        Boolean member = stringRedisTemplate.opsForSet().isMember(BLOG_LIKE_KEY + likeBlog.getUserId(), likeBlog.getBlogId());
+        String blogId = String.valueOf(likeBlog.getBlogId());
+        Boolean member = stringRedisTemplate.opsForSet().isMember(BLOG_LIKE_KEY + likeBlog.getUserId(), blogId);
         //如果用户之前点过赞
-        if (Boolean.TRUE.equals(member)) {
+        if (BooleanUtil.isTrue(member)) {
             //取消点赞
             LambdaQueryWrapper<TLikeBlog> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(TLikeBlog::getBlogId, likeBlog.getBlogId());
@@ -50,16 +52,16 @@ public class TLikeBlogServiceImpl extends ServiceImpl<TLikeBlogMapper, TLikeBlog
                 //更新blog的点赞数量
                 boolean update = blogService.update().setSql("like_count = like_count - 1").eq("id", likeBlog.getBlogId()).update();
                 if (update) {
-                    Long remove1 = stringRedisTemplate.opsForSet().remove(BLOG_COMMENT_KEY + likeBlog.getUserId(), likeBlog.getBlogId());
+                    Long remove1 = stringRedisTemplate.opsForSet().remove(BLOG_LIKE_KEY + likeBlog.getUserId(), blogId);
                     return remove1 != null && remove1 > 0 ? Result.success("已取消点赞") : Result.error("网络异常");
                 }
             }
             return Result.error("网络异常");
         }
         //用户之前没点过赞，查看用户是否反对过该文章
-        Boolean oppose = stringRedisTemplate.opsForSet().isMember(BLOG_OPPOSE_KEY + likeBlog.getUserId(), likeBlog.getBlogId());
+        Boolean oppose = stringRedisTemplate.opsForSet().isMember(BLOG_OPPOSE_KEY + likeBlog.getUserId(), blogId);
         //如果用户没反对过
-        if (!Boolean.TRUE.equals(oppose)) {
+        if (BooleanUtil.isFalse(oppose)) {
             //如果用户也没反对过该文章，查询数据库，排除redis和数据库数据不一致的情况
             LambdaQueryWrapper<TLikeBlog> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(TLikeBlog::getBlogId, likeBlog.getBlogId());
@@ -75,7 +77,7 @@ public class TLikeBlogServiceImpl extends ServiceImpl<TLikeBlogMapper, TLikeBlog
                     boolean update = blogService.update().setSql("like_count = like_count + 1").eq("id", likeBlog.getBlogId()).update();
                     if (update) {
                         //向redis中添加用户的点赞信息
-                        Long add = stringRedisTemplate.opsForSet().add(BLOG_OPPOSE_KEY + likeBlog.getUserId(), String.valueOf(likeBlog.getBlogId()));
+                        Long add = stringRedisTemplate.opsForSet().add(BLOG_LIKE_KEY+ likeBlog.getUserId(), String.valueOf(likeBlog.getBlogId()));
                         return add != null && add > 0 ? Result.success("点赞成功") : Result.error("网络异常");
                     }
                     return Result.error("网络异常");
@@ -88,14 +90,14 @@ public class TLikeBlogServiceImpl extends ServiceImpl<TLikeBlogMapper, TLikeBlog
         }
         //用户之前反对该文章
         //更新数据库信息，更新redis
-        boolean update1 = this.update().setSql("isLike = 1").
+        boolean update1 = this.update().setSql("is_like = 1").
                 eq("blog_id", likeBlog.getBlogId()).eq("user_id", likeBlog.getUserId()).update();
         //更新成功，写入redis
         if(update1){
             //删除用户反对set的数据
-            stringRedisTemplate.opsForSet().remove(BLOG_OPPOSE_KEY + likeBlog.getUserId(), likeBlog.getBlogId());
+            stringRedisTemplate.opsForSet().remove(BLOG_OPPOSE_KEY + likeBlog.getUserId(), blogId);
             //向用户点赞set添加数据
-            stringRedisTemplate.opsForSet().add(BLOG_OPPOSE_KEY + likeBlog.getUserId(), String.valueOf(likeBlog.getBlogId()));
+            stringRedisTemplate.opsForSet().add(BLOG_LIKE_KEY + likeBlog.getUserId(), String.valueOf(likeBlog.getBlogId()));
             return Result.success("点赞成功");
         }
         return Result.error("网络异常");
